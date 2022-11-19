4dff0b9395a7fdfc53168a321394f086021b505a
ne006 <IATikhomirov@gmail.com>
2022-11-19 16:44:18 +0300

    ExpressionAST::Grammar::Boolean

---
+++ lib/expression_ast/grammar/boolean.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "expression_ast/grammar/boolean/node"
+require "expression_ast/grammar/boolean/group"
+
+require "expression_ast/grammar/boolean/operators/conjunction"
+require "expression_ast/grammar/boolean/operators/disjunction"
+
+module ExpressionAST
+  module Grammar
+    module Boolean
+      class << self
+        def literal
+          ExpressionAST::Grammar::Boolean::Node
+        end
+
+        def group
+          ExpressionAST::Grammar::Boolean::Group
+        end
+
+        def operators
+          [
+            [self::Operators::Conjunction],
+            [self::Operators::Disjunction]
+          ]
+        end
+      end
+    end
+  end
+end
---
+++ spec/expression_ast/grammar/boolean_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "expression_ast/grammar/boolean"
+
+RSpec.describe ExpressionAST::Grammar::Boolean do
+  subject(:grammar) { described_class }
+
+  describe ".literal" do
+    it "should return Boolean::Node" do
+      expect(grammar.literal).to be(described_class::Node)
+    end
+  end
+
+  describe ".group" do
+    it "should return Boolean::Group" do
+      expect(grammar.group).to be(described_class::Group)
+    end
+  end
+
+  describe ".operators" do
+    it "should return Boolean operators grouped by their priority" do
+      expect(grammar.operators).to eql(
+        [
+          [described_class::Operators::Conjunction],
+          [described_class::Operators::Disjunction]
+        ]
+      )
+    end
+  end
+end
