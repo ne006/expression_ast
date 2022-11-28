037a9997dec571135e3d62136bac5a2130e74bac
i.tikhomirov <i.tikhomirov@rambler-co.ru>
2022-11-28 11:26:23 +0300

    Add DSL methods to Base::Node

--- lib/expression_ast/base/node.rb
+++ lib/expression_ast/base/node.rb
@@ -3,18 +3,38 @@
 module ExpressionAST
   module Base
     class Node
+      class << self
+        def parse_value(&block)
+          return @parse_value unless block_given?
+
+          @parse_value = block
+        end
+
+        def result(&block)
+          return @result unless block_given?
+
+          @result = block
+        end
+
+        def stringify(&block)
+          return @stringify unless block_given?
+
+          @stringify = block
+        end
+      end
+
       attr_reader :value
 
       def initialize(value)
-        @value = value
+        @value = self.class.parse_value ? self.class.parse_value.call(value) : value
       end
 
       def result
-        value
+        self.class.result ? self.class.result.call(value) : value
       end
 
       def to_s
-        value.to_s
+        self.class.stringify ? self.class.stringify.call(value) : value.to_s
       end
 
       def ==(other)
